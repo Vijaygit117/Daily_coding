2ed05a37121c283f5d1cd5dc8a3b9907b4fcb2b3
Vijaygit117 <vijayakumarsakthivel07@gmail.com>
2025-07-19 09:51:44 +0530

    Initial commit

---
+++ src/Armstrong.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Armstrong {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int original=a;
+        int sum=0;
+        int amg=0;
+        while(a>0){
+            sum=a%10;
+            amg+=sum*sum*sum;
+            a/=10;
+        }
+        if(amg==original){
+            System.out.println("Armstrong");
+        }
+        else
+        {
+            System.out.println("not a Armstrong");
+        }
+    }
+}
---
+++ src/sample.java
@@ -0,0 +1,7 @@
+public class sample {
+    public static void main(String[] args) {
+        int max,min=0;
+        max=10;
+        System.out.println(max+" "+min);
+    }
+}

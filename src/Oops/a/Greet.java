b182bb8d6c2363a9db4a52b2e1d6f79f58f8b879
Vijaygit117 <vijayakumarsakthivel07@gmail.com>
2025-07-20 18:54:42 +0530

    Changes made on 20/07/2025

---
+++ src/Oops/a/Greet.java
@@ -0,0 +1,12 @@
+package Oops.a;
+
+public class Greet {
+    public static void main(String[] args) {
+       message();
+    }
+     static void message(){
+
+        System.out.println("Hello");
+    }
+
+}
